89e7d66bcf3d85477636f468765c977957557928
ankush9850 <75522537+ankush9850@users.noreply.github.com>
2022-02-23 12:24:32 +0530

    Initial commit for math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+# Add Implementation 
+def add(x,y):
+    return x+y 
+
+# Subtract Implementation 
+def subtract(x,y):
+    return x-y              # on master branch
+
+# Multiply Implementation 
+def multiply(x,y):
+    return x*y              # on Bug456 branch
+
+# Divide Implementation 
+def divide(x,y):
+    if y == 0:                      # on master branch 
+        return DIVIDE_BY_0_ERROR
+    else:
+        return x/y 
+
+# Square Implementation 
+def square(x):
+    return x*x  
